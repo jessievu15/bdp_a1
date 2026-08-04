26efa4f61f5663e674a170ad6434c885d71996bc
jessievu15 <khuengan15112003@gmail.com>
2026-08-04 14:56:42 +1000

    in-mapper combining implementation

--- mapper_v2.py
+++ mapper_v2.py
@@ -2,49 +2,38 @@
 import sys
 from collections import defaultdict
 
-trip_type_dict = defaultdict(set)
-#  empty list of total_trips, total_fare, fare_list, total_distance
-taxi_dict = defaultdict(lambda: [0, 0.0, [], 0.0])
-trip_type_list = ["short", "medium", "long"]
+#  initialise empty list of trip_count, max_fare, min_fare, total_fare
+taxi_dict = defaultdict(lambda: [0, None, None, 0.0])
 
 for line in sys.stdin:
     line = line.strip() # remove leading/trailing whitespace
     fields = line.split(',') # split into the fields
 
     # don't need pickup and dropoff information for now
-    trip_id = fields[0]
     taxi_id = fields[1] 
     fare = float(fields[2])
     distance = float(fields[3])
 
-    # check: all distance > 0
     if distance < 100:
-        trip_type_dict[taxi_id].add(trip_type_list[0])
-        taxi_dict[(taxi_id, trip_type_list[0])][0] += 1
-        taxi_dict[(taxi_id, trip_type_list[0])][1] += fare
-        taxi_dict[(taxi_id, trip_type_list[0])][2].append(fare)
-        taxi_dict[(taxi_id, trip_type_list[0])][3] += distance
+        trip_type = "short"
     elif distance >= 100 and distance < 200:
-        trip_type_dict[taxi_id].add(trip_type_list[1])
-        taxi_dict[(taxi_id, trip_type_list[1])][0] += 1
-        taxi_dict[(taxi_id, trip_type_list[1])][1] += fare
-        taxi_dict[(taxi_id, trip_type_list[1])][2].append(fare)
-        taxi_dict[(taxi_id, trip_type_list[1])][3] += distance
+        trip_type = "medium"
     else:
-        trip_type_dict[taxi_id].add(trip_type_list[2])
-        taxi_dict[(taxi_id, trip_type_list[2])][0] += 1
-        taxi_dict[(taxi_id, trip_type_list[2])][1] += fare
-        taxi_dict[(taxi_id, trip_type_list[2])][2].append(fare)
-        taxi_dict[(taxi_id, trip_type_list[2])][3] += distance
+        trip_type = "long"
 
-for (taxi_id, trip_type), (total_trip, total_fare, fare_list, total_distance) in taxi_dict.items():
-    print(f"{taxi_id}\t{trip_type}\t{total_trip}\t{max(fare_list)}\t{min(fare_list)}\t{total_fare}\t{total_distance}")
+    taxi_dict[(taxi_id, trip_type)][0] += 1 # update trip count
 
+    if taxi_dict[(taxi_id, trip_type)][1] is None or fare > taxi_dict[(taxi_id, trip_type)][1]:
+        # if max_fare is None or fare is greater than max_fare, update max_fare
+        taxi_dict[(taxi_id, trip_type)][1] = fare
+    if taxi_dict[(taxi_id, trip_type)][2] is None or fare < taxi_dict[(taxi_id, trip_type)][2]:
+        # if min_fare is None or fare is less than min_fare, update min_fare
+        taxi_dict[(taxi_id, trip_type)][2] = fare
 
-# taxiID that does not have all 3 trip types, the missing types presented as zero values for the categories
-for taxi_id, trip_type in trip_type_dict.items():
-    if len(trip_type) != 3:
-        for type in trip_type_list:
-            if type not in trip_type:
-                print(f"{taxi_id}\t{type}\t0\t0.0\t0.0\t0.0\t0.0")
+    taxi_dict[(taxi_id, trip_type)][3] += fare # update total fare
+    
+
+for (taxi_id, trip_type), (trip_count, max_fare, min_fare, total_fare) in taxi_dict.items():
+    print(f"{taxi_id}\t{trip_type}\t{trip_count}\t{max_fare}\t{min_fare}\t{total_fare}")
+
 
