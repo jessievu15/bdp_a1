e55f9bac3e5e68b9e4780979d66ae3dd667ed640
Zuin Yeong <s2113758@student.rmit.edu.au>
2026-08-21 12:30:23 +1000

    Implemented run shell for Task 3 Job 1 to run in Hadoop

---
+++ task3-job1-reducer.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import sys
+
+current_taxi_id = None
+company = None
+
+for line in sys.stdin:
+    line = line.strip()
+    if not line:
+        continue
+
+    taxi_id, tag, data = line.split('\t')
+
+    # Reset current taxi when reading new taxi id
+    if taxi_id != current_taxi_id:
+        current_taxi_id = taxi_id
+        company = None
+
+    # check if tag for the line is "A" - means its from Taxis.txt
+    if tag == "A":
+        fields = data.split('\t')
+        company = fields[0]
+
+    elif tag == "B":
+        fields = data.split('\t')
+        fare = float(fields[0])
+        distance = float(fields[1])
+
+        if company is not None:
+            print(f"{taxi_id}\t{company}\t{fare}\t{distance}")
+
+
+
+
+
+
+
+
+
