e771b01f0e8a25f84d60d01b03beb990ccfe68db
Zuin Yeong <s2113758@student.rmit.edu.au>
2026-08-22 21:38:43 +1000

    - Removed need to use os library by hard coding logic based on length of fields - Added comments for clarity

--- task3-job1-mapper.py
+++ task3-job1-mapper.py
@@ -1,30 +1,30 @@
 #!/usr/bin/env python3
 import sys
-import os
-
-# Get the name of the file currently being processed by Hadoop.
-filepath = os.environ.get('mapreduce_map_input_file', '').lower()
 
 for line in sys.stdin:
     line = line.strip()  # remove leading/trailing whitespace
     if not line:  # in case there are empty lines at start or end
         continue
 
-    fields = line.split(',')  # split into the fields
+    fields = line.split(',')  # split into fields
 
-    # Check which input dataset current line is from
-    if "taxis" in filepath:
-        # Input 1: Taxis.txt (only need taxi id and company)
+    # Check which input file the current line is from
+    # data from Taxis.txt will have 4 fields when split
+    # collect taxi id and company data
+    if len(fields) == 4:
         taxi_id = fields[0]
         company = fields[1]
+
         # Tag data from Taxis.txt as A
         print(f"{taxi_id}\tA\t{company}")
 
+    # data from Trips.txt will have 8 fields when split
+    # collect taxi id, fare and distance
     else:
-        # Input 2: Trips.txt (only need taxi id, fare and distance)
         taxi_id = fields[1]
         fare = fields[2]
         distance = fields[3]
+
         # Tag data from Trips.txt as B
         print(f"{taxi_id}\tB\t{fare}\t{distance}")
 
