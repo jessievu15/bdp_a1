ccb58ff4a4070e08913b00f7a3f6f7b0ca47d411
Zuin Yeong <s2113758@student.rmit.edu.au>
2026-08-24 21:34:24 +1000

    - changed to use a list rather than a dictionary - included a set to capture unique taxi ids for each company - corrected logic to ensure fleet count is collated correctly

--- task3-job2-reducer.py
+++ task3-job2-reducer.py
@@ -2,14 +2,13 @@
 import sys
 
 #  initialise empty dict
-#  company_dict to contain
-#  (company):[total revenue, total trips, fleet size, revenue per taxi, avg trip distance]
-company_dict = {}
-current_company = 0
-current_taxi = 0
-fleet = 0
+#  company_totals to contain
+#  [total revenue, total trips, fleet size, total distance]
+company_totals = []
+current_company = None
 
-
+# use a set to keep track of unique taxi ids for a company
+current_company_taxis = set()
 
 #  input from job 1 taxi id, company, fare and distance
 for line in sys.stdin:
@@ -24,25 +23,37 @@ for line in sys.stdin:
     total_fare = float(fields[3])
     total_distance = float(fields[4])
 
-    if company not in company_dict:
-        # company_dict values = [total rev, total trips, fleet size, total dist, rev per taxi, avg trip dist]
-        company_dict[company] = [0.0, 0, 0, 0.0, 0.0, 0.0]
+    # capture the company_data for each new company read in.
+    # shuffle and sort will be set up to sort records with same company to same reducer.
+    if current_company is not None and current_company != company:
+        total_rev, total_trips, fleet_size, total_distance = company_totals
 
-    # work out totals for company
-    if current_taxi != taxi_id:
-        company_dict[company][0] += total_fare
-        company_dict[company][1] += trips
-        company_dict[company][2] += 1
-        company_dict[company][3] += total_distance
+        # calculate averages
+        rev_per_taxi = total_rev / fleet_size
+        avg_trip_dist = total_distance / total_trips
 
-    current_taxi = taxi_id
+        print(f"{current_company}\t{total_rev:.2f}\t{fleet_size}\t{total_distance:.2f}\t{rev_per_taxi:.2f}\t{avg_trip_dist:.2f}")
 
-# work out the averages
-for company in company_dict:
-    company_dict[company][4] += (company_dict[company][0] / company_dict[company][2])
-    company_dict[company][5] += (company_dict[company][3] / company_dict[company][1])
-    print(f"{company}\t{company_dict[company][0]:.2f}\t{company_dict[company][1]}\t{company_dict[company][2]}\t{company_dict[company][3]:.2f}\t{company_dict[company][4]:.2f}\t{company_dict[company][5]:.2f}")
+        # reset the list to prepare for next company.
+        company_totals = [0.0, 0, 0, 0.0]
+        # reset the taxi set
+        current_company_taxis = set()
+
     current_company = company
 
+    # Aggregate company totals
+    company_totals[0] += total_fare
+    company_totals[1] += trips
+    company_totals[3] += total_distance
 
+    if taxi_id not in current_company_taxis:
+        current_company_taxis.add(taxi_id)
+        company_totals[2] += 1  # increase fleet count for each new taxi in company
+
+if current_company:
+    total_rev, total_trips, fleet_size, total_dist = company_totals
+    rev_per_taxi = total_rev / fleet_size
+    avg_trip_dist = total_dist / total_trips
+
+    print(f"{current_company}\t{total_rev:.2f}\t{total_trips}\t{fleet_size}\t{total_dist:.2f}\t{rev_per_taxi:.2f}\t{avg_trip_dist:.2f}")
 
