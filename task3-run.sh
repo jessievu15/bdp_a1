48c3faf17a8423137b03f7edd9bfa1d2be555efa
jessievu15 <khuengan15112003@gmail.com>
2026-08-26 10:33:48 +1000

    task3 job3 with 1 mapper

--- task3-run.sh
+++ task3-run.sh
@@ -31,15 +31,15 @@ hadoop jar /usr/lib/hadoop/hadoop-streaming.jar \
 -input /Output/task3_job1 \
 -output /Output/task3_job2
 
-hadoop fs -getmerge /Output/task3_job2 job2_output.txt
 
 hadoop jar /usr/lib/hadoop/hadoop-streaming.jar \
+-D mapreduce.job.reduces=1 \
 -D mapreduce.job.reduces=3 \
 -D stream.num.map.output.key.fields=3 \
 -D mapred.text.key.partitioner.options=-k1,1 \
 -D mapred.output.key.comparator.class=org.apache.hadoop.mapred.lib.KeyFieldBasedComparator \
 -D mapred.text.key.comparator.options='-k3,3nr' \
--files ./task3_job3_mapper.py,./task3_job3_reducer.py,./job2_output.txt \
+-files ./task3_job3_mapper.py,./task3_job3_reducer.py \
 -mapper ./task3_job3_mapper.py \
 -reducer ./task3_job3_reducer.py \
 -input /Output/task3_job2 \
@@ -47,8 +47,7 @@ hadoop jar /usr/lib/hadoop/hadoop-streaming.jar \
 -partitioner org.apache.hadoop.mapred.lib.KeyFieldBasedPartitioner
 
 # remove any intermediate output
-hadoop fs -rm -r /Output/task3_job1
-hadoop fs -rm -r /Output/task3_job2
+
 
 hadoop fs -ls /Output/task3
 hadoop fs -getmerge /Output/task3/part* task3_output.txt
--- task3_job3_mapper.py
+++ task3_job3_mapper.py
@@ -4,22 +4,6 @@ import sys
 company_dict = {}
 total_revenue_list = []
 
-
-with open("job2_output.txt") as f:
-    for line in f:
-        line = line.strip()
-        if not line:
-            continue
-        fields = line.split('\t')
-        total_revenue_list.append(float(fields[1]))
-
-# calculate the cutoff benchmark to separate the record into 3 revenue level: high (0), medium (1), low (2)
-min_rev = min(total_revenue_list)
-max_rev = max(total_revenue_list)
-band = (max_rev - min_rev)/3
-cutoff_high = max_rev - band
-cutoff_low = min_rev + band
-
 def revenue_level(total_revenue):
     
     if total_revenue >= cutoff_high:
@@ -40,6 +24,17 @@ for line in sys.stdin:
     rev_per_taxi = float(fields[4])
     avg_trip_dist = float(fields[5])
 
+    total_revenue_list.append(total_revenue)
+    company_dict[company_id] = [total_revenue, total_trips, fleet_size, rev_per_taxi, avg_trip_dist]
+
+# calculate the cutoff benchmark to separate the record into 3 revenue level: high (0), medium (1), low (2)
+min_rev = min(total_revenue_list)
+max_rev = max(total_revenue_list)
+band = (max_rev - min_rev)/3
+cutoff_high = max_rev - band
+cutoff_low = min_rev + band 
+
+for company_id, (total_revenue, total_trips, fleet_size, rev_per_taxi, avg_trip_dist) in company_dict.items():
     level = revenue_level(total_revenue)
     print(f"{level}\t{company_id}\t{total_revenue}\t{total_trips}\t{fleet_size}\t{rev_per_taxi}\t{avg_trip_dist}")
 
